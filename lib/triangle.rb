f3d8f669aa6ca6849ccc6f3b43550fd1fb83cebd
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-17 17:41:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,8 @@ class Triangle
       :equilateral
     elsif @side_one == @side_two && @side_one != @side_three || @side_one == @side_three && @side_one != @side_two || @side_two == @side_three && @side_two != @side_one
       :isosceles
+    else
+      :scalene
     end
   end
 
