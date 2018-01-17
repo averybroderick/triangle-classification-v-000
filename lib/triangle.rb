573c38c2a993fd837ebb31ad7c3dbf66f5db91e4
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-17 17:45:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,14 @@ class Triangle
   end
 
   def kind
+
+    if @side_one == 0 || @side_two == 0 || @side_three == 0
+      begin
+       raise PartnerError
+     rescue PartnerError => error
+         puts error.message
+    end
+
     if @side_one == @side_two && @side_two == @side_three
       :equilateral
     elsif @side_one == @side_two && @side_one != @side_three || @side_one == @side_three && @side_one != @side_two || @side_two == @side_three && @side_two != @side_one
